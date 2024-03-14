9db25ffcda48a18dcd144df31f904ebf1bb17848
Austin Tripp <ajt212@cam.ac.uk>
2024-03-14 11:25:07 +0000

    Add more info about why logging levels are set the way they are in main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,7 @@ import logging
 import sys
 
 from retro_fallback_iclr24.iclr24_experiments import analyze_results, run_search
+from retro_fallback_iclr24.metrics import logger as metrics_logger
 from retro_fallback_iclr24.retro_fallback import logger as rfb_logger
 
 if __name__ == "__main__":
@@ -15,9 +16,10 @@ if __name__ == "__main__":
         format="%(asctime)s %(name)s %(levelname)s %(message)s",
         filemode="w",
     )
-    rfb_logger.setLevel(logging.DEBUG - 1)  # show every iteration
-    analyze_results.logger.setLevel(logging.DEBUG)
-    run_search.logger.setLevel(logging.DEBUG)
+    rfb_logger.setLevel(logging.DEBUG - 1)  # show detailed logs of every retro-fallback iteration
+    analyze_results.logger.setLevel(logging.DEBUG)  # show time of each step of analysis
+    run_search.logger.setLevel(logging.DEBUG)  # show info about which molecules are being run
+    metrics_logger.setLevel(logging.DEBUG)  # show info about SSP calculation
 
     # Run search and analyze results
     run_search.run_search_and_analyze_results()
